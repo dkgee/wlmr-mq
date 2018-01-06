39cbab6b243ae7c77c251aa48e51422ab21d936e
jht <dk_gee@163.com>
2018-01-06 10:01:29 +0800

    消息队列系列学习

---
+++ src/main/java/com/dk/WlmrMqApplication.java
@@ -0,0 +1,12 @@
+package com.dk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlmrMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlmrMqApplication.class, args);
+	}
+}
